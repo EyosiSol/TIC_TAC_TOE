b8290c796a25add3c88469560c8ae9aa991af672
EyosiSol <eyosila123@gmail.com>
2024-05-11 17:38:15 +0300

    Finalize

--- script.js
+++ script.js
@@ -22,25 +22,31 @@ playerForm.addEventListener("submit", function (event) {
   const playerOName = document.getElementById("player2").value;
   playerFirst.innerHTML = playerXName;
   playerSecond.innerHTML = playerOName;
-  modal.style.display = "none"; // Hide the modal after form submission
+  modal.style.display = "none"; 
+  startGame();// Hide the modal after form submission
 });
 //END OF MODAL
 
 
 
-
 //THE GAME
-// let Result = document.getElementById('result')
+let score1 = document.querySelector("#scoreOne");
+let score2 = document.querySelector("#scoreTwo");
+
+
 let restartBtn = document.getElementById('reset')
 let boxes = Array.from(document.getElementsByClassName('cell'))
 
 let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winningblock')
 
-const O_TEXT = "O"
+const O_TEXT = "0"
 const X_TEXT = "X"
 let currentPlayer = X_TEXT
 let spaces = Array(9).fill(null)
 let countsPlayed = 0
+
+let playerOneScore = 0
+let playerTwoScore = 0
 
 const startGame = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked))
@@ -55,8 +61,15 @@ function boxClicked(e) {
             // Result.innerHTML = `${currentPlayer} has won!`
             let winning_blocks = playerHasWon()
             countsPlayed=10;
-            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
-            return 
+            if(currentPlayer == X_TEXT){
+                playerOneScore++
+                score1.innerText = playerOneScore;
+            } else {
+                playerTwoScore++
+                score2.innerText = playerTwoScore
+            }
+          
+            return   winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
         }
 
         currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
@@ -64,7 +77,11 @@ function boxClicked(e) {
     }
 
     if(countsPlayed == 9) {
-        playerText.innerHTML = `It's a draw!`
+        playerOneScore++
+        playerTwoScore++
+        score1.innerText = playerOneScore;
+        score2.innerText = playerTwoScore
+        boxes.forEach(box => box.style.backgroundColor = winnerIndicator)
     }
 }
 
@@ -90,5 +107,18 @@ function playerHasWon() {
     return false
 }
 
+function resetGame(){
+    spaces = Array(9).fill(null)
+    boxes.forEach(box => box.innerText = "")
+    boxes.forEach(box => box.style.backgroundColor = "#211A1D")
+    countsPlayed = 0
+    currentPlayer = X_TEXT
+}
 
-startGame()
+
+const cont = document.querySelector("#continue");
+
+cont.addEventListener("click", function () {
+        resetGame();
+        startGame()   
+});
