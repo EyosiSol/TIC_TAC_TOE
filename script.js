490c01426efc88ff709622f2a4b6d62a8a0d8fd8
EyosiSol <eyosila123@gmail.com>
2024-05-11 15:07:31 +0300

    Start the Game part where the clicking will add the letter to the target div

--- script.js
+++ script.js
@@ -5,23 +5,40 @@ reset.addEventListener("click", function () {
 });
 //End of The restart button
 
-//THE MODAL
-const modal = document.querySelector("#modal");
-const start = document.querySelector("#start");
-const playerFirst = document.querySelector("#playerFirst");
-const playerSecond = document.querySelector("#playerSecond");
-const playerForm = document.getElementById("playerForm");
+// //THE MODAL
+// const modal = document.querySelector("#modal");
+// const start = document.querySelector("#start");
+// const playerFirst = document.querySelector("#playerFirst");
+// const playerSecond = document.querySelector("#playerSecond");
+// const playerForm = document.getElementById("playerForm");
 
-window.onload = function () {
-  modal.style.display = "block";
+// window.onload = function () {
+//   modal.style.display = "block";
+// };
+
+// playerForm.addEventListener("submit", function (event) {
+//   event.preventDefault(); // Prevent page refresh
+//   const playerXName = document.getElementById("player1").value;
+//   const playerOName = document.getElementById("player2").value;
+//   playerFirst.innerHTML = playerXName;
+//   playerSecond.innerHTML = playerOName;
+//   modal.style.display = "none"; // Hide the modal after form submission
+// });
+// //END OF MODAL
+
+
+//THE GAME
+const cells = Array.from(document.querySelectorAll(".cell"));
+
+let currentPlayer = "X";
+
+
+const startGame = () => {
+    cells.forEach(cell => cell.addEventListener("click",boxClicked))
 };
 
-playerForm.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent page refresh
-  const playerXName = document.getElementById("player1").value;
-  const playerOName = document.getElementById("player2").value;
-  playerFirst.innerHTML = playerXName;
-  playerSecond.innerHTML = playerOName;
-  modal.style.display = "none"; // Hide the modal after form submission
-});
-//END OF MODAL
+function boxClicked(e){
+    e.target.textContent = currentPlayer;
+};
+
+startGame();
