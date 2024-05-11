f82a7fbd7a1660f407ce90586774867bdf02aeb9
EyosiSol <eyosila123@gmail.com>
2024-05-11 13:32:42 +0300

    add the functionality for the dialog modal

---
+++ script.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector("#modal");
+const start = document.querySelector("#start");
+const playerFirst = document.querySelector("#playerFirst");
+const playerSecond = document.querySelector("#playerSecond");
+
+const playerForm = document.getElementById("playerForm");
+
+window.onload = function() {
+    modal.style.display = "block";
+}
+
+playerForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent page refresh
+    const playerXName = document.getElementById("player1").value;
+    const playerOName = document.getElementById("player2").value;
+    playerFirst.innerHTML = playerXName;
+    playerSecond.innerHTML = playerOName;
+    modal.style.display = "none"; // Hide the modal after form submission
+  });
+  
